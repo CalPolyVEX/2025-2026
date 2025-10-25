b1169f714556e6032958cb24c6b84bfd9fab65c4
Isaac Pruett <121455801+Isaac-Pruett@users.noreply.github.com>
2025-10-25 11:54:00 -0700

    added controller

--- src/chassis.cpp
+++ src/chassis.cpp
@@ -46,7 +46,7 @@ Drive chassis(
     // External ratio, must be in decimal, in the format of input teeth/output teeth.
     // If your motor has an 84-tooth gear and your wheel has a 60-tooth gear, this value will be 1.4.
     // If the motor drives the wheel directly, this value is 1:
-    0.625,
+    0.2222222222222222,
 
     // Gyro scale, this is what your gyro reads when you spin the robot 360 degrees.
     // For most cases 360 will do fine here, but this scale factor can be very helpful when precision is necessary.
