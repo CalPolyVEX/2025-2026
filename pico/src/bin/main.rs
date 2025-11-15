3fd83fafe93c0bb58b24d142ee7a139cb8c9b740
Isaac Pruett <121455801+Isaac-Pruett@users.noreply.github.com>
2025-11-14 19:08:29 -0800

    modified main.rs, added example.yaml

--- pico/src/bin/main.rs
+++ pico/src/bin/main.rs
@@ -9,12 +9,13 @@
 use embassy_executor::Spawner;
 use embassy_rp::bind_interrupts;
 use embassy_rp::gpio;
-use embassy_rp::peripherals::USB;
+//use embassy_rp::peripherals::USB;
+use defmt_rtt as _; // global logger
 use embassy_rp::usb::{Driver, InterruptHandler};
 use embassy_time::Timer;
 use gpio::{Level, Output};
 use log::info;
-use panic_halt as _; // <- provides #[panic_handler]
+use panic_probe as _; // panic handler
 
 // use {defmt_rtt as _, panic_probe as _};
 
@@ -31,23 +32,11 @@ pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
     embassy_rp::binary_info::rp_program_build_attribute!(),
 ];
 
-bind_interrupts!(struct Irqs {
-    USBCTRL_IRQ => InterruptHandler<USB>;
-});
-
-#[embassy_executor::task]
-async fn logger_task(driver: Driver<'static, USB>) {
-    embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver);
-}
-
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
 
     let mut led = Output::new(p.PIN_25, Level::Low);
-
-    let driver = Driver::new(p.USB, Irqs);
-    spawner.spawn(logger_task(driver)).unwrap();
 
     loop {
         info!("led on!");
